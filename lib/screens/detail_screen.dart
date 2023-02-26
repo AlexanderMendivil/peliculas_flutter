6f7a38884ca682d475e43a3821e65974bc7f1d47
Alexander Mendivil <alexanderima@outlook.es>
2023-02-26 15:09:07 -0700

    detail screen appbar

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -4,12 +4,41 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final String movie = ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+    
     return Scaffold(
-      body: Container(
-        child: const Center(
-          child: Text('Detail Screen'),
-        ),
-      ),
+      body: CustomScrollView(
+        slivers: [
+          _CustomAppBar()
+        ],
+        )
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.amber,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true, 
+        titlePadding: EdgeInsets.all(0),
+        title: Container(
+          child: Text('Movie.title', style: TextStyle(fontSize: 16),),
+          color: Colors.black12,
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+
+          ),
+        background: FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'), 
+          image: NetworkImage('https://via.placeholder.com/500x400'),
+          fit: BoxFit.cover,),),
     );
   }
 }
