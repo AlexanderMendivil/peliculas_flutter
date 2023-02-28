3f956a23ee15a785a9d4540e2b770615bc79326b
Alexander Mendivil <alexanderima@outlook.es>
2023-02-27 20:07:50 -0700

    detail movie widget

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -9,7 +9,10 @@ class DetailsScreen extends StatelessWidget {
     return Scaffold(
       body: CustomScrollView(
         slivers: [
-          _CustomAppBar()
+          _CustomAppBar(),
+          SliverList(delegate: SliverChildListDelegate([
+            const _PosterAndTitle()
+          ]))
         ],
         )
     );
@@ -27,18 +30,60 @@ class _CustomAppBar extends StatelessWidget {
       pinned: true,
       flexibleSpace: FlexibleSpaceBar(
         centerTitle: true, 
-        titlePadding: EdgeInsets.all(0),
+        titlePadding: const EdgeInsets.all(0),
         title: Container(
-          child: Text('Movie.title', style: TextStyle(fontSize: 16),),
           color: Colors.black12,
           width: double.infinity,
           alignment: Alignment.bottomCenter,
+          child: const Text('Movie.title', style: TextStyle(fontSize: 16),),
 
           ),
-        background: FadeInImage(
+        background: const FadeInImage(
           placeholder: AssetImage('assets/loading.gif'), 
           image: NetworkImage('https://via.placeholder.com/500x400'),
           fit: BoxFit.cover,),),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  const _PosterAndTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final textTheme = Theme.of(context).textTheme;
+    return Container(
+      margin: const EdgeInsets.only( top: 20 ),
+      padding: const EdgeInsets.symmetric( horizontal: 20 ),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: 
+            BorderRadius.circular( 20 ),
+            child: const FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'),
+              image: NetworkImage('https://via.placeholder.com/200x300'),
+              height: 150,
+               ),
+            ),
+            const SizedBox(width: 20,),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start, 
+              children: [
+                Text('Movie.title', style: textTheme.headline5, overflow: TextOverflow.ellipsis, maxLines: 2,),
+                Text('Movie.originalTitle', style: textTheme.subtitle1, overflow: TextOverflow.ellipsis, ),
+                Row(
+                  children: [
+                    const  Icon(Icons.star_outline, size: 15, color: Colors.grey,),
+                    const SizedBox( width: 5 ,),
+                    Text('movie.voteAverage', style: textTheme.caption ,)
+                  ]
+                  ,)
+
+              ],)
+          ],
+      ),
     );
   }
 }
